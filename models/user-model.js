4eeb2406fd18298d13a8f4dcfffc8137813060d7
Forge <mohamedabdeltawab338@gmail.com>
2024-04-14 21:47:11 +0200

    Updated user database model

--- models/user-model.js
+++ models/user-model.js
@@ -3,8 +3,6 @@ const { default: mongoose } = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  firstname: { type: String },
-  lastname: { type: String },
   username: { type: String, required: true },
   password: { type: String, required: true },
   membership: {
