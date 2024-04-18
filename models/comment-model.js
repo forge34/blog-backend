b894541f485872828e5fdec51c6aa68c1e291058
Forge <>
2024-04-18 23:42:08 +0200

    Updated database models

--- models/comment-model.js
+++ models/comment-model.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  body:{type:String , required:true},
-  author:{type:Schema.ObjectId}
-})
+    body: { type: String, required: true },
+    author: { type: Schema.ObjectId },
+    comment_date: { type: Date, default: Date.now },
+});
 
-const CommentModel = mongoose.model("Comment" , CommentSchema)
+const CommentModel = mongoose.model("Comment", CommentSchema);
 
-module.exports = CommentModel
+module.exports = CommentModel;
--- models/user-model.js
+++ models/user-model.js
@@ -3,17 +3,17 @@ const { default: mongoose } = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  membership: {
-    type: String,
-  },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    membership: {
+        type: String,
+    },
+    isAdmin: { type: Boolean },
 });
 
 UserSchema.virtual("fullname").get(function () {
-  return `${this.firstname} ${this.lastname}`;
+    return `${this.firstname} ${this.lastname}`;
 });
-
 
 const userModel = mongoose.model("User", UserSchema);
 module.exports = userModel;
